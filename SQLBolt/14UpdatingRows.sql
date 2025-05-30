b10369f05248d2d6deb1bb31fcd4e8efa93149bf
Balym <b_sekenova@kbtu.kz>
2025-05-30 18:33:43 +0500

    Updating rows
    Funny how it is easy, I thought it was second hardest thing in the databases course haha

---
+++ SQLBolt/14UpdatingRows.sql
@@ -0,0 +1,68 @@
+CREATE TABLE Movies(
+    Id INT PRIMARY KEY,
+    Title VARCHAR(255),
+    Director VARCHAR(255),
+    Year INT,
+    Length_minutes INT
+);
+
+CREATE TABLE Boxoffice(
+    Movie_id INT PRIMARY KEY,
+    Rating FLOAT,
+    Domestic_sales INT,
+    International_sales INT
+);
+
+INSERT INTO Movies
+VALUES (1, 'Toy Story', 'John Lasseter', 1995, 81),
+       (2, 'A Bug''s Life','John Lasseter', 1998, 95 ),
+       (3, 'Toy Story 2', 'John Lasseter', 1999, 93),
+      (4, 'Monsters, Inc.', 'Pete Docter', 2001, 92),
+      (5, 'Finding Nemo', 'Andrew Stanton', 2003, 107),
+      (6, 'The Incredibles', 'Brad Bird', 2004, 116),
+      (7, 'Cars', 'John Lasseter', 2006, 117),
+      (8, 'Ratatouille', 'Brad Bird', 2007, 115),
+      (9, 'WALL-E', 'Andrew Stanton', 2008, 104),
+      (10, 'Up', 'Pete Docter', 2009, 101),
+      (11, 'Toy Story 8', 'El Directore', 2010, 103),
+      (12, 'Cars 2', 'John Lasseter', 2011, 120),
+      (13, 'Brave', 'Brenda Chapman', 2012, 102),
+      (14, 'Monsters University', 'Dan Scanlon', 2013, 110),
+      (15, 'Toy Story 4', 'Balym Sekenova', 2025, 110);
+
+INSERT INTO Boxoffice
+VALUES(5, 8.2, 380843261, 555900000),
+      (14,7.4,268492764,475066843),
+        (8,8,206445654,417277164),
+        (12,6.4,191452396,368400000),
+        (3,7.9,245852179,239163000),
+        (6,8,261441092,370001000),
+        (9,8.5,223808164,297503696),
+        (11,8.4,415004880,648167031),
+        (1,8.3,191796233,170162503),
+        (7,7.2,244082982,217900167),
+        (10,8.3,293004164,438338580),
+        (4,8.1,289916256,272900000),
+        (2,7.2,162798565,200600000),
+        (13,7.2,237283207,301700000),
+        (15, 8.7, 340000000, 270000000);
+
+/*Update statement with values
+UPDATE mytable
+SET column = value_or_expr,
+    other_column = another_value_or_expr,
+    …
+WHERE condition;*/
+
+UPDATE movies
+SET director = 'John Lasseter'
+WHERE title = 'A Bug''s Life';  /*The director for A Bug's Life is incorrect, it was actually directed by John Lasseter*/
+
+UPDATE movies
+SET year = 1999
+WHERE title = 'Toy Story 2'; /*The year that Toy Story 2 was released is incorrect, it was actually released in 1999*/
+
+UPDATE movies
+SET title = 'Toy Story 3',
+director = 'Lee Unkrich'
+WHERE id = 11; /*Both the title and director for Toy Story 8 is incorrect! The title should be "Toy Story 3" and it was directed by Lee Unkrich*/
