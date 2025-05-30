f65c7bb04b2048e85943b0f60f58d8b54888fce7
Balym <b_sekenova@kbtu.kz>
2025-05-30 18:40:59 +0500

    Creating tables
    Well I meann I have been doing it, but info about types of data was usefull

---
+++ SQLBolt/16CreatingTables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Database(
+Name VARCHAR(255),
+Version FLOAT,
+Download_count INT);
+
+/*Create a new table named Database with the following columns:
+– Name A string (text) describing the name of the database
+– Version A number (floating point) of the latest version of this database
+– Download_count An integer count of the number of times this database was downloaded
+This table has no constraints. */
